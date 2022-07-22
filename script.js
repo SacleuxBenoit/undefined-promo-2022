e8d0c387371a54466fcaa6df39fe4a76c32b4e5c
SacleuxBenoit <benoitsacleux@outlook.fr>
2022-07-22 16:14:27 +0200

    update cards : we now have 3 cards per cols

---
+++ script.js
@@ -0,0 +1,20 @@
+
+const main = document.querySelector('main');
+
+async function fetchStudents ()
+{
+	const response = await fetch('http://localhost:5500/promo.json').then(response => response.json());
+	const students = response.students;
+
+	students.forEach( student => {
+		let card = document.createElement('div');
+		let p = document.createElement('p');
+		card.className = "text-gray-900 text-lg leading-tight font-medium mb-8 border";
+		card.innerHTML = student.lastname + " " + student.firstname;
+		p.innerHTML = `<a href="https://github.com/${student.githubid}">Github link</a>`
+		main.appendChild(card);
+		card.appendChild(p);
+	});
+}
+
+fetchStudents();
