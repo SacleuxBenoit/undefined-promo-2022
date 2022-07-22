2643f6ab1b4f2a2cfc95a83e9732382b189b03f5
SacleuxBenoit <benoitsacleux@outlook.fr>
2022-07-22 16:37:44 +0200

    create folder images + add github logo instead of the entire link in script.js

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ async function fetchStudents ()
 		let p = document.createElement('p');
 		card.className = "text-gray-900 text-lg leading-tight font-medium mb-8 border";
 		card.innerHTML = student.lastname + " " + student.firstname;
-		p.innerHTML = `<a href="https://github.com/${student.githubid}">Github link</a>`
+		p.innerHTML = `<a href="https://github.com/${student.githubid}" target="_blank"><img src="./images/githubLogo.png" alt="nop"></a>`
 		main.appendChild(card);
 		card.appendChild(p);
 	});
