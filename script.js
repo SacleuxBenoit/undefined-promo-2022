399a7fc4c637eb0e79430f97e3d69c4a403f4bd1
SacleuxBenoit <benoitsacleux@outlook.fr>
2022-07-23 20:01:38 +0200

    add verification : display github link only if student have an account

--- script.js
+++ script.js
@@ -11,7 +11,9 @@ async function fetchStudents ()
 		let p = document.createElement('p');
 		card.className = "text-gray-900 text-lg leading-tight font-medium mb-8 border";
 		card.innerHTML = student.lastname + " " + student.firstname;
-		p.innerHTML = `<a href="https://github.com/${student.githubid}" target="_blank"><img src="./images/githubLogo.png" alt="nop"></a>`
+		if(student.githubid){
+			p.innerHTML = `<a href="https://github.com/${student.githubid}" target="_blank"><img src="./images/githubLogo.png" alt="nop"></a>`
+		}
 		main.appendChild(card);
 		card.appendChild(p);
 	});
