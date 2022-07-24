cf32f48feb69a5a4a0eec528c7dd688e4594d94b
SacleuxBenoit <benoitsacleux@outlook.fr>
2022-07-24 18:49:55 +0200

    add verification for see if username && lastname exist + change color depand on sex for each student in :arrow_right: script.js

--- script.js
+++ script.js
@@ -10,9 +10,20 @@ async function fetchStudents ()
 		let card = document.createElement('div');
 		let p = document.createElement('p');
 		card.className = "text-gray-900 text-lg leading-tight font-medium mb-8 border";
-		card.innerHTML = student.lastname + " " + student.firstname;
+
+		if(student.lastname && student.firstname && student.sex == "M"){
+			card.className = "text-blue-800 text-lg leading-tight font-medium mb-8 border";
+			card.innerHTML = student.lastname + " " + student.firstname;
+		}else if(student.lastname && student.firstname && student.sex == "F"){
+			card.className = "text-pink-800 text-lg leading-tight font-medium mb-8 border";
+			card.innerHTML = student.lastname + " " + student.firstname;
+		}
+		else{
+			card.innerHTML = "there is no username or lastname";
+		}
+
 		if(student.githubid){
-			p.innerHTML = `<a href="https://github.com/${student.githubid}" target="_blank"><img src="./images/githubLogo.png" alt="nop"></a>`
+			p.innerHTML = `<a href="https://github.com/${student.githubid}" target="_blank"><img src="./images/githubLogo.png" alt="github image"></a>`
 		}
 		main.appendChild(card);
 		card.appendChild(p);
