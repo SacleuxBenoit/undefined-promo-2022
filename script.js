66e36e4f69b50c0f4af5af008a742b205944fdaa
SacleuxBenoit <benoitsacleux@outlook.fr>
2022-07-26 15:37:19 +0200

    :memo: change background-color for body + cars, && update size of github logo + add new github logo (dark && white)

--- script.js
+++ script.js
@@ -13,10 +13,10 @@ async function fetchStudents ()
 		card.className = "text-gray-900 text-lg leading-tight font-medium mb-8 border";
 		// verify if username && lastname exist for student + change color depand on the sex
 		if(student.lastname && student.firstname && student.sex == "M"){
-			card.className = "text-blue-800 text-lg leading-tight font-medium mb-8 border";
+			card.className = "text-blue-800 text-lg leading-tight font-medium mb-8 border bg-slate-900 w-3/4 ml-20 pl-4 pt-2 text-center";
 			card.innerHTML = student.lastname + " " + student.firstname;
 		}else if(student.lastname && student.firstname && student.sex == "F"){
-			card.className = "text-pink-800 text-lg leading-tight font-medium mb-8 border";
+			card.className = "text-pink-800 text-lg leading-tight font-medium mb-8 border bg-slate-900 w-3/4 ml-20 pl-4 pt-2 text-center";
 			card.innerHTML = student.lastname + " " + student.firstname;
 		} // if there is no username && lastname display a message
 		else{
@@ -25,7 +25,7 @@ async function fetchStudents ()
 
 		// display github link only if user have an account
 		if(student.githubid){
-			p.innerHTML = `<a href="https://github.com/${student.githubid}" target="_blank"><img src="./images/githubLogo.png" alt="github image"></a>`
+			p.innerHTML = `<a href="https://github.com/${student.githubid}" target="_blank"><img src="./images/githubLogoWhite.png" alt="github image" class="pt-2 mb-2 w-8"></a>`
 		}
 		main.appendChild(card);
 		card.appendChild(p);
