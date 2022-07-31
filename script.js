c7f7b1b1c5237b1d830c5633a8169b21385ef361
SacleuxBenoit <benoitsacleux@outlook.fr>
2022-07-31 21:33:13 +0200

    update background color for body && cards + text-color for cards in :arrow_right: index.html && script.js

--- script.js
+++ script.js
@@ -13,10 +13,10 @@ async function fetchStudents ()
 		card.className = "text-gray-900 text-lg leading-tight font-medium mb-8 border";
 		// verify if username && lastname exist for student + change color depand on the sex
 		if(student.lastname && student.firstname && student.sex == "M"){
-			card.className = "text-blue-800 text-lg leading-tight font-medium mb-8 border bg-slate-900 w-3/4 ml-20 pl-4 pt-2 text-center";
+			card.className = "text-blue-700 text-lg leading-tight font-medium mb-8 border bg-slate-900 w-3/4 ml-20 pl-4 pt-2 text-center";
 			card.innerHTML = student.lastname + " " + student.firstname;
 		}else if(student.lastname && student.firstname && student.sex == "F"){
-			card.className = "text-pink-800 text-lg leading-tight font-medium mb-8 border bg-slate-900 w-3/4 ml-20 pl-4 pt-2 text-center";
+			card.className = "text-pink-700 text-lg leading-tight font-medium mb-8 border bg-slate-900 w-3/4 ml-20 pl-4 pt-2 text-center";
 			card.innerHTML = student.lastname + " " + student.firstname;
 		} // if there is no username && lastname display a message
 		else{
